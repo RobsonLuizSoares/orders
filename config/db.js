412e688bc544ab17470e6f6b16f9270c9376ad31
RobsonLuizSoares <robsonptrainer@gmail.com>
2019-06-19 12:19:30 -0300

    add new cluster netlink

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 require('dotenv')
 if(process.env.NODE_ENV == 'production'){
-    module.exports = { mongoURI: 'mongodb+srv://lya:lya250916@cluster0-yjaky.mongodb.net/test?retryWrites=true&w=majority'}
+    module.exports = { mongoURI: 'mongodb+srv://lya:lya250916@netlink-rfdt7.mongodb.net/test?retryWrites=true&w=majority'}
 }else{
     module.exports = {mongoURI: 'mongodb://localhost/netlink'}
 }
